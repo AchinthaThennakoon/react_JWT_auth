dee7e34aebf3b719af90bdd182e74f2b9f2d3bd8
achintha <achinthathennakoon97@gmail.com>
2022-06-06 01:14:49 +0530

    created home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import NavbarComp from "./components/NavbarComp";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <NavbarComp />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+function Home() {
+  return (
+    <div>
+      <Card>
+        <Card.Body>you are not logged in</Card.Body>
+      </Card>
+    </div>
+  );
+}
+
+export default Home;
