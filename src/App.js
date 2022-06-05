0ec0e1b5db8b299c100622046255b92827b6f7c7
achintha <achinthathennakoon97@gmail.com>
2022-06-06 00:24:32 +0530

    import react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
 
 function App() {
   return <div className="App"></div>;
