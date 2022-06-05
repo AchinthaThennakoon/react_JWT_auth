0a4bc938a428ca6b8b238975a349c94e27996176
achintha <achinthathennakoon97@gmail.com>
2022-06-06 01:43:05 +0530

    set router paths

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import "./App.css";
 import NavbarComp from "./components/NavbarComp";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import Home from "./components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <NavbarComp />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavbarComp />
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
