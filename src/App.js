1cbfd0b8a2baf897239d989828c02ad503fe470a
achintha <achinthathennakoon97@gmail.com>
2022-06-06 00:41:46 +0530

    created navbar component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
 import "./App.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">Home</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Login</Nav.Link>
-              <Nav.Link href="#link">Sign up</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
+
+function Navbar() {
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Home</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Login</Nav.Link>
+              <Nav.Link href="#link">Sign up</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Navbar;
