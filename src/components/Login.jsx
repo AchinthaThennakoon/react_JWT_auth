5687ae26c0f9f559f1fb739f0c8761d1d8e4e7eb
achintha <achinthathennakoon97@gmail.com>
2022-06-07 01:34:01 +0530

    created login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Login() {
-  return <div>Login</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      email: email,
+      password: password,
+    };
+
+    axios
+      .post(`https://jsonplaceholder.typicode.com/users`, { data })
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+      });
+  };
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <form className="col-8 offset-2 " onClick={handleSubmit}>
+      <h3>Login</h3>
+
+      <div className="form-group">
+        <label>Email</label>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Enter email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Enter password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+
+      <button type="submit" className="btn btn-dark btn-lg btn-block">
+        Login
+      </button>
+      <p className="forgot-password text-right">
+        Not registered <Link to="/register">Register?</Link>
+      </p>
+    </form>
+  );
 }
 
 export default Login;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import userServives from "./userServives";
 
 function Register() {
   const handleSubmit = (e) => {
