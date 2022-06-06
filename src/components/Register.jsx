d3d8fdae2a27beff7b417db80edd3a993aff3496
achintha <achinthathennakoon97@gmail.com>
2022-06-06 16:23:21 +0530

    create post request using axios

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,19 +1,86 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Register() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { id, value } = e.target;
+    if (id === "firstName") {
+      setFirstName(value);
+    }
+    if (id === "lastName") {
+      setLastName(value);
+    }
+    if (id === "email") {
+      setEmail(value);
+    }
+    if (id === "password") {
+      setPassword(value);
+    }
+    if (id === "confirmPassword") {
+      setConfirmPassword(value);
+    }
+
+    console.log(
+      firstName +
+        " " +
+        lastName +
+        " " +
+        email +
+        " " +
+        password +
+        " " +
+        confirmPassword
+    );
+
+    axios
+      .post("/register", {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+      })
+      .then((Response) => {
+        console.log(Response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   return (
     <form className="col-8 offset-2 ">
       <h3>Register</h3>
 
       <div className="form-group">
         <label>First name</label>
-        <input type="text" className="form-control" placeholder="First name" />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="First name"
+          id="firstName"
+          value={firstName}
+          onChange={(e) => handleSubmit(e)}
+        />
       </div>
 
       <div className="form-group">
         <label>Last name</label>
-        <input type="text" className="form-control" placeholder="Last name" />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Last name"
+          id="lastName"
+          value={lastName}
+          onChange={(e) => handleSubmit(e)}
+        />
       </div>
 
       <div className="form-group">
@@ -22,6 +89,9 @@ function Register() {
           type="email"
           className="form-control"
           placeholder="Enter email"
+          id="email"
+          value={email}
+          onChange={(e) => handleSubmit(e)}
         />
       </div>
 
@@ -31,6 +101,9 @@ function Register() {
           type="password"
           className="form-control"
           placeholder="Enter password"
+          id="password"
+          value={password}
+          onChange={(e) => handleSubmit(e)}
         />
       </div>
 
@@ -40,10 +113,17 @@ function Register() {
           type="password"
           className="form-control"
           placeholder="confirm password"
+          id="confirmPassword"
+          value={confirmPassword}
+          onChange={(e) => handleSubmit(e)}
         />
       </div>
 
-      <button type="submit" className="btn btn-dark btn-lg btn-block">
+      <button
+        type="submit"
+        className="btn btn-dark btn-lg btn-block"
+        onClick={handleSubmit}
+      >
         Register
       </button>
       <p className="forgot-password text-right">
